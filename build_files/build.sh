14d9ce9561a74871055d084d21d52af86be654c4
Lander Brandt <hello@landaire.net>
2026-08-20 01:17:25 -0700

    feat: add nix mountpoint

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+#dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -24,4 +24,7 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+
+### nix Mountpoint
+mkdir -p /nix
